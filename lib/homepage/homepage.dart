4507ee6e4661548a5c5eddf40886816bc0d67647
fachdf <57356502+fachdf@users.noreply.github.com>
2023-09-14 15:01:21 +0700

    update login page
    + anonymous login
    + sign in with google
    - bug

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -1,13 +1,14 @@
 import 'package:dhiahydroponics/shared/bottomnavbar.dart';
 import 'package:flutter/material.dart';
-
+import 'package:dhiahydroponics/services/auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavBar(),
+      bottomNavigationBar: BottomNavBar()
     );
   }
 }
--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,3 +1,4 @@
+import 'package:dhiahydroponics/services/auth.dart';
 import 'package:dhiahydroponics/shared/bottomnavbar.dart';
 import 'package:flutter/material.dart';
 
@@ -7,8 +8,16 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      bottomNavigationBar: BottomNavBar(),
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: ElevatedButton(
+          child: Text('signout'),
+          onPressed: () async {
+            await AuthService().signOut();
+            Navigator.of(context)
+                .pushNamedAndRemoveUntil('/', (route) => false);
+          }),
     );
   }
 }
